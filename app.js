d3e72990ddf32a3dc1d8fd6f6c06cae7400dfe6d
Dunia Ndewon <dunia@ndewon.com>
2019-11-17 17:58:42 +0700

    Create error handler middleware

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const session = require('express-session');
-const passport = require('passport');
-const User = require('./models/user');
+const session = require("express-session");
+const passport = require("passport");
+const User = require("./models/user");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 /** define routes */
-const indexRouter = require('./routes/index');
-const postsRouter = require('./routes/posts');
-const reviewsRouter = require('./routes/reviews');
+const indexRouter = require("./routes/index");
+const postsRouter = require("./routes/posts");
+const reviewsRouter = require("./routes/reviews");
 
 const app = express();
 
@@ -21,7 +21,7 @@ const app = express();
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      'mongodb+srv://testapi:helloworld@test-api-szxff.mongodb.net/test?retryWrites=true&w=majority',
+      "mongodb+srv://testapi:helloworld@test-api-szxff.mongodb.net/test?retryWrites=true&w=majority",
       {
         useNewUrlParser: true,
         useCreateIndex: true,
@@ -30,7 +30,7 @@ const connectDB = async () => {
       }
     );
 
-    console.log('database is connected!');
+    console.log("database is connected!");
   } catch (err) {
     console.error(err.message);
     process.exit(1);
@@ -39,19 +39,19 @@ const connectDB = async () => {
 connectDB();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 /** configure passport and sessions */
 app.use(
   session({
-    secret: 'ganbarooo',
+    secret: "ganbarooo",
     resave: false,
     saveUninitialized: true
   })
@@ -63,9 +63,9 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 /** mount routes */
-app.use('/', indexRouter);
-app.use('/posts', postsRouter);
-app.use('/posts/:id/reviews', reviewsRouter);
+app.use("/", indexRouter);
+app.use("/posts", postsRouter);
+app.use("/posts/:id/reviews", reviewsRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -76,11 +76,11 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,58 +1,59 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { postRegister } = require('../controllers/index');
+const { postRegister } = require("../controllers");
+const { errorHandler } = require("../middleware");
 
 /* GET home page. */
-router.get('/', (req, res) => {
-  res.render('index', { title: 'Surf Shop - home' });
+router.get("/", (req, res) => {
+  res.render("index", { title: "Surf Shop - home" });
 });
 
 /** GET /register */
-router.get('/register', (req, res) => {
-  res.send('GET /register');
+router.get("/register", (req, res) => {
+  res.send("GET /register");
 });
 
 /** POST /register */
-router.post('/register', postRegister);
+router.post("/register", errorHandler(postRegister));
 
 /** GET /login */
-router.get('/login', (req, res) => {
-  res.send('GET /login');
+router.get("/login", (req, res) => {
+  res.send("GET /login");
 });
 
 /** POST /login */
-router.post('/login', (req, res) => {
-  res.send('POST /login');
+router.post("/login", (req, res) => {
+  res.send("POST /login");
 });
 
 /** GET /profile */
-router.get('/profile', (req, res) => {
-  res.send('GET /profile');
+router.get("/profile", (req, res) => {
+  res.send("GET /profile");
 });
 
 /** PUT /profile/:user_id */
-router.put('/profile/:user_id', (req, res) => {
-  res.send('PUT /profile/:user_id');
+router.put("/profile/:user_id", (req, res) => {
+  res.send("PUT /profile/:user_id");
 });
 
 /** GET /forgot-pw */
-router.get('/forgot-pw', (req, res) => {
-  res.send('GET /forgot-pw');
+router.get("/forgot-pw", (req, res) => {
+  res.send("GET /forgot-pw");
 });
 
 /** PUT /forgot-pw */
-router.put('/forgot-pw', (req, res) => {
-  res.send('PUT /forgot-pw');
+router.put("/forgot-pw", (req, res) => {
+  res.send("PUT /forgot-pw");
 });
 
 /** GET /reset-pw */
-router.get('/reset-pw/:token', (req, res) => {
-  res.send('GET /reset-pw/:token');
+router.get("/reset-pw/:token", (req, res) => {
+  res.send("GET /reset-pw/:token");
 });
 
 /** PUT /reset-pw */
-router.put('/reset-pw/:token', (req, res) => {
-  res.send('PUT /reset-pw/:token');
+router.put("/reset-pw/:token", (req, res) => {
+  res.send("PUT /reset-pw/:token");
 });
 
 module.exports = router;
